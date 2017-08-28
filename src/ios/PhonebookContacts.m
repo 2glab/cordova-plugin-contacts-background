d6587d308a86b623e4b5bf11365139b5eb87dbe1
Shaliko Usubov <shaliko@ezid.ru>
2017-08-28 12:28:00 +0300

    fetching data from phonebook.

--- src/ios/PhonebookContacts.m
+++ src/ios/PhonebookContacts.m
@@ -1,3 +1,4 @@
+#import <Contacts/Contacts.h>
 #import "PhonebookContacts.h"
 #import <Cordova/CDV.h>
 
@@ -21,19 +22,80 @@
 {
     // Check command.arguments here.
     [self.commandDelegate runInBackground:^{
-        NSString* payload = nil;
+        __block NSString* payload = nil;
         // Some blocking logic...
-        for (int i = 1; i <= 1000000000; i++)
-        {
-            int r = arc4random_uniform(i);
-            if(i % 100000000 == 0) {
-                NSLog(@"%d", i);
+//        for (int i = 1; i <= 100000000; i++)
+//        {
+//            int r = arc4random_uniform(i);
+//            if(i % 50000000 == 0) {
+//                NSLog(@"%d", i);
+//            }
+//        }
+
+        /////////////
+        if ([CNContactStore class]) {
+            //ios9 or later
+            CNEntityType entityType = CNEntityTypeContacts;
+            if( [CNContactStore authorizationStatusForEntityType:entityType] == CNAuthorizationStatusNotDetermined)
+            {
+                CNContactStore * contactStore = [[CNContactStore alloc] init];
+                [contactStore requestAccessForEntityType:entityType completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                    if(granted){
+                        [self getAllContact];
+                    }
+                }];
+            }
+            else if( [CNContactStore authorizationStatusForEntityType:entityType]== CNAuthorizationStatusAuthorized)
+            {
+                [self getAllContact];
             }
         }
+        /////////////
+
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
         // The sendPluginResult method is thread-safe.
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
+}
+
+-(void)getAllContact
+{
+    if([CNContactStore class])
+    {
+        //iOS 9 or later
+        NSError* contactError;
+        CNContactStore* addressBook = [[CNContactStore alloc]init];
+        [addressBook containersMatchingPredicate:[CNContainer predicateForContainersWithIdentifiers: @[addressBook.defaultContainerIdentifier]] error:&contactError];
+        NSArray * keysToFetch =@[CNContactEmailAddressesKey, CNContactPhoneNumbersKey, CNContactFamilyNameKey, CNContactGivenNameKey, CNContactPostalAddressesKey];
+        CNContactFetchRequest * request = [[CNContactFetchRequest alloc]initWithKeysToFetch:keysToFetch];
+        BOOL success = [addressBook enumerateContactsWithFetchRequest:request error:&contactError usingBlock:^(CNContact * __nonnull contact, BOOL * __nonnull stop){
+            [self parseContactWithContact:contact];
+        }];
+    }
+}
+
+- (void)parseContactWithContact :(CNContact* )contact
+{
+    NSString * firstName =  contact.givenName;
+    NSString * lastName =  contact.familyName;
+    NSString * phone = [[contact.phoneNumbers valueForKey:@"value"] valueForKey:@"digits"];
+    NSString * email = [contact.emailAddresses valueForKey:@"value"];
+    NSArray * addrArr = [self parseAddressWithContac:contact];
+
+    NSLog(@" phone => %@", phone);
+}
+
+- (NSMutableArray *)parseAddressWithContac: (CNContact *)contact
+{
+    NSMutableArray * addrArr = [[NSMutableArray alloc]init];
+    CNPostalAddressFormatter * formatter = [[CNPostalAddressFormatter alloc]init];
+    NSArray * addresses = (NSArray*)[contact.postalAddresses valueForKey:@"value"];
+    if (addresses.count > 0) {
+        for (CNPostalAddress* address in addresses) {
+            [addrArr addObject:[formatter stringFromPostalAddress:address]];
+        }
+    }
+    return addrArr;
 }
 
 @end
